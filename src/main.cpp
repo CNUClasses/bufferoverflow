4659f304d56e30922d7f5e6915dfedfda27d4621
Keith <kperkins411@gmail.com>
2017-11-21 12:24:32 -0500

    buffer overflow demo with gnu C++ compiler

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 21, 2017
+ *      Author: keith
+ */
+#include <string.h>
+#include <cstdio>
+
+int main(){
+	int valid = 0;
+	
+	//note the proximity of these 2 variable allocations
+	//they are also layed out in memory one after another!
+	//(use memory view to look at them)
+	char password_fromuser[ 8 ];
+	char password_CORRECT[ 8 ];
+	
+	//the password I am looking for
+	strcpy( password_CORRECT, "START" ); // the correct password
+	
+	//gets a password from user, expects <=8 chars
+	//if I instead enter 17 or 18 1s, I will 
+	//enter 8 1's as password_fromuser 
+	//overwrite password_CORRECT with 8 1's
+	gets( password_fromuser );
+
+	//look at memory again and see that password_CORRECT is all 1's
+	if (strncmp( password_CORRECT, password_fromuser, 8) == 0) {
+		//and I'm in!
+		valid = 1;
+	}
+	printf("expected(%s) actual(%s) valid(%d)\n", password_CORRECT, password_fromuser, valid);
+	return 0;
+}
+
+
+
+
